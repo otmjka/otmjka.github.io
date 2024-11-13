aaefdd0c149c1b36cb85a8386743cc6413fec49a
otmjka <otmjka@gmail.com>
2024-11-13 20:42:50 +0100

    feat(vite): replace index.html to src; build to root for github pages easy commit;

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
+import process from 'node:process'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: '/',
+  },
+  root: 'src',
+  publicDir: '../public',
+  build: {
+    outDir: '../',
+  },
+  resolve: {
+    alias: { '/src': path.resolve(process.cwd(), 'src') },
+  },
 })
