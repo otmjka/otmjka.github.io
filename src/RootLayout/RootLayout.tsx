e6f1047349eb7e5bb7c81ae7f1854072eb653030
otmjka <otmjka@gmail.com>
2024-11-27 08:36:34 +0100

    fix(router): lowered App below router provider

--- src/RootLayout/RootLayout.tsx
+++ src/RootLayout/RootLayout.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react'
-import { Outlet } from 'react-router-dom'
-import { Link } from 'react-router-dom'
+import { Outlet, Link } from 'react-router-dom'
 import { Box, Container } from '@mui/material'
+import { App } from '../App'
 
 import {
   AppTheme,
@@ -12,17 +12,19 @@ import packageJson from '../../package.json'
 
 const RootLayout: FC = () => {
   return (
-    <AppTheme>
-      {/* <AppAppBar logoLink={Link} /> */}
-      <>
-        <Link to="/">rndmsd0</Link> [{packageJson.version}]
-      </>
-      <Box pt={7}>
-        <Container fixed>
-          <Outlet />
-        </Container>
-      </Box>
-    </AppTheme>
+    <App>
+      <AppTheme>
+        {/* <AppAppBar logoLink={Link} /> */}
+        <>
+          <Link to="/">rndmsd0</Link> [{packageJson.version}]
+        </>
+        <Box pt={7}>
+          <Container fixed>
+            <Outlet />
+          </Container>
+        </Box>
+      </AppTheme>
+    </App>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { App } from './App'
+
 import appRouter from './appRouter'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App>
-      <RouterProvider router={appRouter} />
-    </App>
+    <RouterProvider router={appRouter} />
   </StrictMode>
 )
