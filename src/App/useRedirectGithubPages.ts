63b0f355311857ca080a758e4a0bc69c792f4bcd
otmjka <otmjka@gmail.com>
2024-11-27 08:26:32 +0100

    feat(routing): redirect in direct links

--- src/App/useRedirectGithubPages.ts
+++ src/App/useRedirectGithubPages.ts
@@ -1,10 +1,14 @@
+import { useNavigate } from 'react-router-dom'
+
 const useRedirectGithubPages = () => {
+  const navigate = useNavigate()
   const redirect = sessionStorage.getItem('redirect')
   console.log('###', redirect)
-  //   if (redirect) {
-  //     sessionStorage.removeItem('redirect')
-  //     history.replaceState(null, null, redirect)
-  //   }
+  if (redirect) {
+    sessionStorage.removeItem('redirect')
+    navigate(redirect)
+    //     history.replaceState(null, null, redirect)
+  }
 }
 
 export default useRedirectGithubPages
